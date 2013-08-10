7fbe24cdb3ac3df18f216b57d2ab9a9d0a8929f5
Marty Kraft <marty.kraft@mosaic.com>
2013-08-10 10:09:55 -0400

    More style changes.

--- lib/fides/postgresql_writer.rb
+++ lib/fides/postgresql_writer.rb
@@ -1,76 +1,90 @@
 module Fides
-
   class PostgresqlWriter
     include SqlWriter
 
-    def self.executable_add_statements(interface_name, models, polymorphic_model)
-      sql = create_and_update_constraint_sql(interface_name, models, polymorphic_model)
-      sql << delete_constraint_sql(interface_name, models, polymorphic_model)
+    def self.executable_add_statements(interface, models, polymorphic_model)
+      sql = create_and_update_sql(interface, models, polymorphic_model)
+      sql << delete_constraint_sql(interface, models, polymorphic_model)
       [sql]
     end
 
-    def self.executable_remove_statements(interface_name)
-      [drop_constraints_sql(interface_name)]
+    def self.executable_remove_statements(interface)
+      [drop_constraints_sql(interface)]
     end
 
     private
 
-    def self.create_and_update_constraint_sql(interface_name, models, polymorphic_model)
+    def self.create_and_update_sql(interface, models, polymorphic_model)
 
-      sql = "DROP FUNCTION IF EXISTS check_#{interface_name}_create_integrity() CASCADE;"
+      sql = "DROP FUNCTION IF EXISTS check_#{interface}_create_integrity() 
+            CASCADE;"
 
       sql << %{
-        CREATE FUNCTION check_#{interface_name}_create_integrity() RETURNS TRIGGER AS '
+        CREATE FUNCTION check_#{interface}_create_integrity() RETURNS 
+        TRIGGER AS '
           BEGIN
-            IF NEW.#{interface_name}_type = ''#{models[0]}'' AND EXISTS (
-                SELECT id FROM #{models[0].constantize.table_name} WHERE id = NEW.#{interface_name}_id) THEN
+            IF NEW.#{interface}_type = ''#{models[0]}'' AND EXISTS (
+                SELECT id FROM #{models[0].constantize.table_name} 
+                WHERE id = NEW.#{interface}_id) THEN
               RETURN NEW;
       }
 
       models[1..-1].each do |model|
         sql << %{
-          ELSEIF NEW.#{interface_name}_type = ''#{model}'' AND EXISTS (
-              SELECT id FROM #{model.constantize.table_name} WHERE id = NEW.#{interface_name}_id) THEN
+          ELSEIF NEW.#{interface}_type = ''#{model}'' AND EXISTS (
+              SELECT id FROM #{model.constantize.table_name} 
+              WHERE id = NEW.#{interface}_id) THEN
             RETURN NEW;
         }
       end
       
       sql << %{
             ELSE
-              RAISE EXCEPTION ''No % model with id %.'', NEW.#{interface_name}_type, NEW.#{interface_name}_id;
+              RAISE EXCEPTION ''No % model with id %.'', 
+              NEW.#{interface}_type, NEW.#{interface}_id;
               RETURN NULL;
             END IF;
           END'
           LANGUAGE plpgsql;
 
-          CREATE TRIGGER check_#{interface_name}_create_integrity_trigger 
-            BEFORE INSERT OR UPDATE ON #{polymorphic_model.constantize.table_name} 
-            FOR EACH ROW EXECUTE PROCEDURE check_#{interface_name}_create_integrity();
+          CREATE TRIGGER check_#{interface}_create_integrity_trigger 
+          BEFORE INSERT OR UPDATE ON #{polymorphic_model.constantize.table_name} 
+          FOR EACH ROW 
+          EXECUTE PROCEDURE check_#{interface}_create_integrity();
 
       }
 
       strip_non_essential_spaces(sql)
     end
 
-    def self.delete_constraint_sql(interface_name, models, polymorphic_model)
+    def self.delete_constraint_sql(interface, models, polymorphic_model)
       polymorphic_model_table_name = polymorphic_model.constantize.table_name
       
       sql = ""
       sql << %{
-        CREATE FUNCTION check_#{interface_name}_delete_integrity() RETURNS TRIGGER AS '
+        CREATE FUNCTION check_#{interface}_delete_integrity() RETURNS 
+        TRIGGER AS '
           BEGIN
-            IF TG_TABLE_NAME = ''#{models[0].constantize.table_name}'' AND EXISTS (
+            IF TG_TABLE_NAME = ''#{models[0].constantize.table_name}'' AND 
+            EXISTS (
               SELECT id FROM #{polymorphic_model_table_name} 
-              WHERE #{interface_name}_type = ''#{models[0]}'' AND #{interface_name}_id = OLD.id) THEN
-              RAISE EXCEPTION ''There are records in #{polymorphic_model_table_name} that refer to %. You must delete those records first.'', OLD;
+              WHERE #{interface}_type = ''#{models[0]}'' 
+              AND #{interface}_id = OLD.id) THEN
+              RAISE EXCEPTION ''There are records in 
+              #{polymorphic_model_table_name} that refer to %. You must delete 
+              those records first.'', OLD;
       }
 
       models[1..-1].each do |model|
         sql << %{
-          ELSEIF TG_TABLE_NAME = ''#{model.constantize.table_name}'' AND EXISTS (
+          ELSEIF TG_TABLE_NAME = ''#{model.constantize.table_name}'' AND EXISTS 
+          (
             SELECT id FROM #{polymorphic_model_table_name} 
-            WHERE #{interface_name}_type = ''#{model}'' AND #{interface_name}_id = OLD.id) THEN
-              RAISE EXCEPTION ''There are records in #{polymorphic_model_table_name} that refer to %. You must delete those records first.'', OLD;
+            WHERE #{interface}_type = ''#{model}'' 
+            AND #{interface}_id = OLD.id) THEN
+              RAISE EXCEPTION ''There are records in 
+              #{polymorphic_model_table_name} that refer to %. You must delete 
+              those records first.'', OLD;
         }
       end
 
@@ -88,21 +102,22 @@ module Fides
         sql << %{
           CREATE TRIGGER check_#{table_name}_delete_integrity_trigger 
             BEFORE DELETE ON #{table_name} 
-            FOR EACH ROW EXECUTE PROCEDURE check_#{interface_name}_delete_integrity();
+            FOR EACH ROW EXECUTE PROCEDURE 
+            check_#{interface}_delete_integrity();
         }
       end
 
       strip_non_essential_spaces(sql)
     end
 
-    def self.drop_constraints_sql(interface_name)
+    def self.drop_constraints_sql(interface)
       sql = %{
-        DROP FUNCTION IF EXISTS check_#{interface_name}_create_integrity() CASCADE;
-        DROP FUNCTION IF EXISTS check_#{interface_name}_delete_integrity() CASCADE;
+        DROP FUNCTION IF EXISTS check_#{interface}_create_integrity() 
+        CASCADE;
+        DROP FUNCTION IF EXISTS check_#{interface}_delete_integrity() 
+        CASCADE;
       }
       strip_non_essential_spaces(sql)
     end
-
-  end #class
-
-end #module
+  end
+end
--- lib/fides/sqlite3_writer.rb
+++ lib/fides/sqlite3_writer.rb
@@ -1,40 +1,40 @@
 require 'fides/sql_writer'
 
 module Fides
-
   class Sqlite3Writer
     include SqlWriter
 
-    def self.executable_add_statements(interface_name, models, polymorphic_model)
+    def self.executable_add_statements(interface, models, polymorphic_model)
       statements = []
-      statements << drop_constraint_sql(interface_name, "create")
-      statements << create_constraint_sql(interface_name, models, polymorphic_model)
+      statements << drop_sql(interface, "create")
+      statements << create_sql(interface, models, polymorphic_model)
       models.each do |model|
-        statements << drop_constraint_sql(model.constantize.table_name, "delete")
-        statements << delete_constraint_sql(interface_name, model, polymorphic_model)
+        statements << drop_sql(model.constantize.table_name, "delete")
+        statements << delete_sql(interface, model, polymorphic_model)
       end
-      statements << drop_constraint_sql(interface_name, "update")
-      statements << update_constraint_sql(interface_name, models, polymorphic_model)
+      statements << drop_sql(interface, "update")
+      statements << update_sql(interface, models, polymorphic_model)
       statements
     end
 
-    def self.executable_remove_statements(interface_name)
+    def self.executable_remove_statements(interface)
       statements = []
-      statements << drop_constraint_sql(interface_name, "create")
-      statements << drop_constraint_sql(interface_name, "delete")
+      statements << drop_sql(interface, "create")
+      statements << drop_sql(interface, "delete")
       statements
     end
 
     private
 
-    def self.drop_constraint_sql(name, drop_type)
-      strip_non_essential_spaces "DROP TRIGGER IF EXISTS check_#{name}_#{drop_type}_integrity;"
+    def self.drop_sql(name, drop_type)
+      strip_non_essential_spaces "DROP TRIGGER IF EXISTS 
+                                  check_#{name}_#{drop_type}_integrity;"
     end
 
-    def self.create_constraint_sql(interface_name, models, polymorphic_model)
+    def self.create_sql(interface, models, polymorphic_model)
         sql = %{
 
-        CREATE TRIGGER check_#{interface_name}_create_integrity
+        CREATE TRIGGER check_#{interface}_create_integrity
           BEFORE INSERT ON #{polymorphic_model.constantize.table_name}
           BEGIN 
             SELECT CASE 
@@ -43,7 +43,7 @@ module Fides
       sql << 'WHEN ('
 
       models.each do |model|  
-        sql << %{NEW.#{interface_name}_type != '#{model}' }
+        sql << %{NEW.#{interface}_type != '#{model}' }
         sql << 'AND ' unless model == models.last
       end
 
@@ -51,8 +51,9 @@ module Fides
 
       models.each do |model|
         sql << %{ 
-          WHEN ((NEW.#{interface_name}_type = '#{model}') AND (SELECT id 
-            FROM #{model.constantize.table_name} WHERE id = NEW.#{interface_name}_id) ISNULL) 
+          WHEN ((NEW.#{interface}_type = '#{model}') AND (SELECT id 
+            FROM #{model.constantize.table_name} 
+            WHERE id = NEW.#{interface}_id) ISNULL) 
             THEN RAISE(ABORT, 'There is no #{model} with that id.') 
         }
       end
@@ -62,10 +63,10 @@ module Fides
       strip_non_essential_spaces(sql)
     end
 
-      def self.update_constraint_sql(interface_name, models, polymorphic_model)
+      def self.update_sql(interface, models, polymorphic_model)
         sql = %{
 
-        CREATE TRIGGER check_#{interface_name}_update_integrity
+        CREATE TRIGGER check_#{interface}_update_integrity
           BEFORE UPDATE ON #{polymorphic_model.constantize.table_name}
           BEGIN 
             SELECT CASE 
@@ -74,7 +75,7 @@ module Fides
       sql << 'WHEN ('
 
       models.each do |model|  
-        sql << %{NEW.#{interface_name}_type != '#{model}' }
+        sql << %{NEW.#{interface}_type != '#{model}' }
         sql << 'AND ' unless model == models.last
       end
 
@@ -82,8 +83,9 @@ module Fides
 
       models.each do |model|
         sql << %{ 
-          WHEN ((NEW.#{interface_name}_type = '#{model}') AND (SELECT id 
-            FROM #{model.constantize.table_name} WHERE id = NEW.#{interface_name}_id) ISNULL) 
+          WHEN ((NEW.#{interface}_type = '#{model}') AND (SELECT id 
+            FROM #{model.constantize.table_name} 
+            WHERE id = NEW.#{interface}_id) ISNULL) 
             THEN RAISE(ABORT, 'There is no #{model} with that id.') 
         }
       end
@@ -93,16 +95,25 @@ module Fides
       strip_non_essential_spaces(sql)
     end
 
-    def self.delete_constraint_sql(interface_name, associated_model, polymorphic_model)
+    def self.delete_sql(interface, associated_model, polymorphic_model)
       sql = %{
 
-        CREATE TRIGGER check_#{associated_model.constantize.table_name}_delete_integrity
+        CREATE TRIGGER 
+          check_#{associated_model.constantize.table_name}_delete_integrity
           BEFORE DELETE ON #{associated_model.constantize.table_name}
           BEGIN
             SELECT CASE
               WHEN ((SELECT id FROM #{polymorphic_model.constantize.table_name} 
-                WHERE #{interface_name}_type = '#{associated_model}' AND #{interface_name}_id = OLD.id) NOTNULL) THEN 
-                  RAISE(ABORT, 'There are records in the #{polymorphic_model.constantize.table_name} table that refer to the #{associated_model.constantize.table_name} record that is attempting to be deleted. Delete the dependent records in the #{polymorphic_model.constantize.table_name} table first.')
+                WHERE #{interface}_type = '#{associated_model}' 
+                AND #{interface}_id = OLD.id) NOTNULL) THEN 
+                  RAISE(ABORT, 
+                    'There are records in the 
+                    #{polymorphic_model.constantize.table_name} 
+                    table that refer to the 
+                    #{associated_model.constantize.table_name} record that is 
+                    attempting to be deleted. Delete the dependent records in 
+                    the #{polymorphic_model.constantize.table_name} table 
+                    first.')
             END;
           END;
 
@@ -110,7 +121,5 @@ module Fides
 
       strip_non_essential_spaces(sql)
     end
-
   end
-
 end
